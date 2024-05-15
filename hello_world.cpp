ba4a82fa7508a1afb90e4a9c68a1e2fca077c1d0
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-05-15 11:02:37 +0200

    variables.cpp added

---
+++ hello_world.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+    //Comment for C++ for oneliners
+    /*
+    Comment for C++ for multiple liners
+    High Level languages: Python, Java, C#
+    Mid level languages: C++, C
+    Low Level languages: Assembly 
+    */
+    std::cout << "Hello World!" << std::endl;
+    std::cout << 1 + 1 << std::endl;
+    return 0;
+}
+
+
---
+++ variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+
+    int x; //declares x as a integer
+    x = 5; //assigning the value 5 to variable x
+    int y = 6; //declaring and assigning in one line
+    int sum = y + x;
+    std::cout << sum << std::endl; //Output of sum {x + y = sum} = {5 + 6 = 11}
+    return 0;
+}
