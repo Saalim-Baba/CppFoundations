dc990f3410909d4d61f6b4fbe6933b7d150bcf0f
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-05-16 10:54:16 +0200

    constants.cpp done

---
+++ constants.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    /*
+    constants are values that are not changeable
+    used for things that shouldn't change. Example: value of speed of light
+    one-time declaration
+    */
+    const std::string author = "milaasHD";
+    return 0;
+}
