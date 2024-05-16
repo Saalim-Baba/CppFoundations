3a8e27ca1f94848d2361d7214dcf4439ea874474
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-05-16 22:13:34 +0200

    indentation fixed

--- namespace.cpp
+++ namespace.cpp
@@ -15,15 +15,15 @@ int main(){
     own seperate namespace.
     */
 
-   int age = 11;
+    int age = 11;
 
-   //Declaring the variable inside function is still possible
-   std::cout << "Last year I was " << age << " years old" << std::endl;
+    //Declaring the variable inside function is still possible
+    std::cout << "Last year I was " << age << " years old" << std::endl;
 
-   //Declaring the variable in the first namespace and refrencing it with {namespace_name}::{variable_name}
-   std::cout << "Today I'm " << first::age << " years old" << std::endl;
-   std::cout << "Next year I'll be " << second::age << " years old" << std::endl;
+    //Declaring the variable in the first namespace and refrencing it with {namespace_name}::{variable_name}
+    std::cout << "Today I'm " << first::age << " years old" << std::endl;
+    std::cout << "Next year I'll be " << second::age << " years old" << std::endl;
 
-
+    
     return 0;
 }
