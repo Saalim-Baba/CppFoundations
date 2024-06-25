7cb400acca4682ffbced122af989899686e69fc2
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-06-25 15:29:09 +0200

    update to userinput

--- user_input.cpp
+++ user_input.cpp
@@ -8,11 +8,12 @@ int main(){
     std::string name;
 
     std::cout << "Your name:";
-    std::getline(std::cin, name);
+    std::getline(std::cin >> std::ws, name);
 
     /* 
     std::cin >> name; if you do it like this and a person could potentially input a string with spaces
     then you must use std::getline(std::cin, name) instead, otherwise it'll use just the first word
+    std::ws is to delete all newline characters or any whitespaces before user_input
     */
    std::cout << name;
 
