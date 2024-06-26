7b132d1b517fc7c5002ed150cf52a8a4d06ac059
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-06-26 21:19:01 +0200

    switch

---
+++ switch.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+int main(){
+    // Switch is used instead of multiple if statements to compare one value
+
+    int month;
+    std::cin >> month;
+
+    switch (month){
+    case 1:
+        std::cout << "January";
+        break;
+    case 2:
+        std::cout << "February";
+        break;
+    case 3:
+        std::cout << "March";
+        break;
+    case 4:
+        std::cout << "April";
+        break;
+    case 5:
+        std::cout << "May";
+        break;
+    case 6:
+        std::cout << "June";
+        break;
+    case 7:
+        std::cout << "July";
+        break;
+    case 8:
+        std::cout << "August";
+        break;
+    case 9:
+        std::cout << "September";
+        break;
+    case 10:
+        std::cout << "October";
+        break;
+    case 11:
+        std::cout << "November";
+        break;
+    case 12:
+        std::cout << "December";
+        break;
+    }
+    return 0;
+}
