e8017fa27cab80c4a47e2636a5fd6ed4f9c66bde
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-06-26 21:07:49 +0200

    else-statement

--- if-statement.cpp
+++ if-statement.cpp
@@ -4,10 +4,16 @@ int main(){
     // If statements give you the options to give conditions, if its true
     // if it doesn't apply, it doesn't do it
 
-    int year = 2024;
+    int year;
+
+    std::cout << "What year do we have: ";
+    std::cin >> year;
     
     if(year == 2024){
-        std::cout << "It's correct, the year given is 2024";
+        std::cout << "It's correct, the year given is " << year;
+    }
+    else{
+        std::cout << "Nope, not the right year buddy";
     };
 
     return 0;
