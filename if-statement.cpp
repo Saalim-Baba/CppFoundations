22351961d16f377c2a5725c923a97c0fe72d9c13
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-06-26 21:12:49 +0200

    instructions for if statements

--- if-statement.cpp
+++ if-statement.cpp
@@ -8,6 +8,11 @@ int main(){
 
     std::cout << "What year do we have: ";
     std::cin >> year;
+
+    /* If using stuff like age like this (if age >= 18) be aware that you are including everything over 18
+    So if you intend to restrict it to under 100 years old, you have to put the if statement for the 
+    100 years first and the 18 in a else if statement so it gets checked first and moves on.
+    */
     
     if(year == 2024){
         std::cout << "It's correct, the year given is " << year;
