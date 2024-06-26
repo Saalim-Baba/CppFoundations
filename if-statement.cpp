9bd57c918659a613e457c5f1bf47531618daaa75
milaasHD <143434800+milaasHD@users.noreply.github.com>
2024-06-26 21:05:14 +0200

    if statements implemented

---
+++ if-statement.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+    // If statements give you the options to give conditions, if its true
+    // if it doesn't apply, it doesn't do it
+
+    int year = 2024;
+    
+    if(year == 2024){
+        std::cout << "It's correct, the year given is 2024";
+    };
+
+    return 0;
+}
